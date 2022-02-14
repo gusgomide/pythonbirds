828de920224848b583bceaf3126ff3c82ceaf346
Gustavo Gomide <Gomidefx@gmail.com>
2022-02-14 18:02:25 -0300

    criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa :
+    olhos = 2
     def __init__(self, *filhos , nome=None, idade=43 ):
         self.idade = idade
         self.nome = nome
@@ -18,6 +19,12 @@ if __name__ == '__main__':
         print(filho.nome)
     gustavo.sobrenome = 'Gomide'
     del gustavo.filhos
+    gustavo.olhos = 1
+    del gustavo.olhos
     print(gustavo.__dict__)
     print(melina.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(gustavo.olhos)
+    print(melina.olhos)
 
